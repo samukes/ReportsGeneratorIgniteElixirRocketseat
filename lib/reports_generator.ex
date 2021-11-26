601f14bb567638a8e1f5c0f1f87e0451af50a6bf
samukes <darkdarumia@gmail.com>
2021-11-26 15:05:24 -0300

    Adding reports resources; Adding a case File open in build funcition to reports_generator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,8 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(filename) do
+    case File.read("reports/#{filename}") do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
   end
 end
